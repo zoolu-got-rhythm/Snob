22e5b7dbb9b6ceec3a504738a2afc87c9602f35e
Christopher P.K Morris <christopher.pkm@gmail.com>
2016-01-13 00:12:23 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// weather API key: 34061ce86bfda355e56b98b6d018e56c
+
+//Problem: Application needs functionality added so user can acquire weather information based on city, state.
+//Solution: Add functionality to get the user the weather information based on the city, state they enter.
+
+//1. User enters city,state into an input field
+  //make a request to openweather api to get data
+  //parse returned data
+//2. when user 'clicks' search button the user receives weather info based on city, state entered.
+
+
+
+
+
+// DOM helper function
+function build(element, width, height) {
+    //  element ? element = element : throw "insert an argument";
+    var newElement = document.createElement(element);
+    document.body.appendChild(newElement);
+    newElement.style.width = width + "px";
+    newElement.style.height = height + "px";
+    newElement.style.backgroundColor = '#ddd';
+    newElement.style.margin = '10px';
+    return newElement;
+}
+
+// create a hundred boxes
+var artists = [{name: "Picasso", pic: "http://thesource.com/wp-content/uploads/2015/02/Pablo_Picasso1.jpg", work: {} }, "warhol", "bob", "jane"];
+var boxes = [];
+for (var i = 0; i < artists.length; i++) {
+    var ele = build("div", "60", "60");
+    var img = getImage(artists[i].pic);
+
+    ele.appendChild(img)
+    boxes.push(ele)
+    // boxes[i].addEventListener("mouseover", function(){ expand(ele, i) });
+    boxes[i].onmouseover = expand.bind(this, (i));
+    boxes[i].onmouseout = detract.bind(this, (i));
+    boxes[i].onclick = gallery.bind(this, (i));
+
+
+}
+
+console.log(boxes);
+
+function expand(currentEle) {
+    boxes[currentEle].style.width = "100px";
+    boxes[currentEle].style.cursor = "pointer";
+
+    boxes[currentEle].innerHTML = "<h3>" + artists[currentEle] + "<h3>";
+    // boxes[currentEle].className = "expand";
+    console.log(boxes[currentEle]);
+    // boxes[currentEle]
+}
+
+
+function detract(currentEle) {
+    boxes[currentEle].style.width = "50px";
+    boxes[currentEle].innerHTML = " ";
+
+    // boxes[currentEle].className = "detract";
+}
+
+function gallery(currentEle) {
+    boxes[currentEle].style.width = window.innerWidth + "px";
+    boxes[currentEle].innerHTML = "workkkkkkkkkk and more work";
+
+    // boxes[currentEle].className = "detract";
+}
+
+// image helper function
+function getImage(url){
+    var img = new Image(55, 55);
+    img.src = url;
+    return img;
+}
